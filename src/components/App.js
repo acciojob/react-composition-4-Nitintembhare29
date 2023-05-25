b7cefe66cd16251835d768ace1b0e30f70dcb921
Nitin Tembhare <nitintembhare340@gmail.com>
2023-05-25 17:24:53 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import Form from './SignUp';
 
 const App = () => {
+  const handleSubmit = (formValues) => {
+    console.log(formValues);
+    // Perform further actions with the form values
+  };
+
+  const fields = [
+    { name: 'name', type: 'text', label: 'Name' },
+    { name: 'email', type: 'email', label: 'Email' },
+    { name: 'password', type: 'password', label: 'Password' }
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Form Example</h1>
+      <Form onSubmit={handleSubmit} fields={fields} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
